cb765f84631fcdf46253e5951b0ba1543b2ce28b
Timothy Charette <charettetimothy@gmail.com>
2018-11-03 10:05:34 -0400

    works

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var config = {
     projectId: "train-scheduler-56cfd",
     storageBucket: "train-scheduler-56cfd.appspot.com",
     messagingSenderId: "632464262085"
-  };
+};
 
 firebase.initializeApp(config);
 var database = firebase.database();
@@ -16,62 +16,46 @@ $("#submit").on("click", function (event) {
     var destination = $("#destination").val().trim();
     var firstTrainTime = $("#firstTrainTime").val().trim();
     var frequency = $("#frequency").val().trim();
-
-    var trainObj= {
+    var trainObj = {
         trainName: trainName,
         destination: destination,
         firstTrainTime: firstTrainTime,
         frequency: frequency
     };
+    database.ref().push(trainObj)
+})
 
-    database.ref().push(trainObj)})
-
-database.ref().on("child_added", function(snapshot,prevKey) {
-
-
-    //Store data in variable
-    var tFreq = snapshot.val().frequency
-    var tDest = snapshot.val().destination
-    var tName = snapshot.val().trainName
-    var tFirstTrain = snapshot.val().firstTrainTime
-
+database.ref().on("child_added", function (snapshot, prevKey) {
+    //Store data in variable 
+    var tFreq = snapshot.val().frequency;
+    var tDest = snapshot.val().destination;
+    var tName = snapshot.val().trainName;
+    var tFirstTrain = snapshot.val().firstTrainTime;
 
     var timeArr = tFirstTrain.split(":");
-    var trainTime = moment.hours(timeArr[0]).minutes(timeArr[1])
+    // console.log(tFirstTrain)
 
-    var maxMoment = moment.max(moment(),"minutes")
-    var tMinutes
-    var tArrival
-
+    var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+    var maxMoment = moment().max(moment(), "minutes");
+    //    console.log(maxMoment, trainTime)
+    var tMinutes;
+    var tArrival;
 
     // if the first train is later than the current time, set the arrival to the first train
-    if (maxMoment === trainTime){
+    if (maxMoment === trainTime) {
+        tArrival = trainTime.format("hh:mm A");
+        tMinutes = trainTime.diff(moment(), "minutes");
+    } else {
+        var differenceTimes = moment().diff(trainTime, "minutes")
 
-            tArrival = trainTime.format("hh:mm A");
-            tMinutes = trainTime.diff(moment(), "minutes")
-            
-
-
-    }else{
-
-        var differenceTimes = moment.diff(trainTime,"minutes")
-        var tRemainder = differenceTimes % tFreq;
-
-        tMinutes = tFrequency - tRemainder
-
-        tArrival = moment().add(tMinutes,"m").format("hh:mm A")
-
+        var tRemainder = Math.abs(differenceTimes % tFreq);
+        tMinutes = tFreq - tRemainder
+        console.log(differenceTimes, tRemainder, tMinutes)
+        tArrival = moment().add(tMinutes, "m").format("hh:mm A")
     }
-
-    // to get next arrival - difference bewtween current and firstTrinTime
-    // then we want to display this to firstTrainTime
-    // so... var diffCurrentFirst = moment()
-
-
-    // to get minutesAway - difference between next arrival and current time
-    $("tbody").append("<tr><th>" + tName 
-    + "</th><th>" + tDest
-    + "</th><th>" + tFreq 
-    + "</th><th>" + tArrival 
-    + "</th><th>" + tMinutes + "</th><th>")
+    $("tbody").append("<tr><th>" + tName +
+        "</th><th>" + tDest +
+        "</th><th>" + tFreq +
+        "</th><th>" + tArrival +
+        "</th><th>" + tMinutes + "</th><th>")
 })
